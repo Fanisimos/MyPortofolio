aa091a6cba3c8c532543e66092adb72cb03e0f9a
John Litsai <xspot1995@hotmail.com>
2025-05-23 13:29:12 +0100

    Minor changes (ignore)

--- src/Portofolio.jsx
+++ src/Portofolio.jsx
@@ -2,24 +2,31 @@ import portofolioItems from "./portofolioItems.js";
 
 function Portofolio() {
   return (
-    <section id="portofolio">
-      <h2>Projects</h2>
-      <div className="portofolio">
-        {portofolioItems.map((item) => (
-          <div
-            className="portofolio-item"
-            key={item.id}
-            onClick={() => item.link} // has to be a function, because of onClick
-          >
-            <a href={item.link} target="_blank" rel="noopener noreferrer">
-              <img src={item.image} alt={item.title} />
-              <h3>{item.title}</h3>
-              <p>{item.description}</p>
-            </a>
-          </div>
-        ))}
-      </div>
-    </section>
+    <>
+      <section id="portofolio">
+        <h2>Projects</h2>
+        <div className="portofolio">
+          {portofolioItems.map((item) => (
+            <div
+              className="portofolio-item"
+              key={item.id}
+              onClick={() => item.link} // has to be a function, because of onClick
+            >
+              <a href={item.link} target="_blank" rel="noopener noreferrer">
+                <img src={item.image} alt={item.title} />
+                <h3>{item.title}</h3>
+                <p>{item.description}</p>
+              </a>
+            </div>
+          ))}
+        </div>
+      </section>
+      {/*FIXME: How to add the components?*/}
+      <section id="portofolio">
+        <h2>Components</h2>
+        <div className="portofolio"> </div>
+      </section>
+    </>
   );
 }
 export default Portofolio;
